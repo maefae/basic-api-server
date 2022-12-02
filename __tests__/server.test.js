490e8cd27bff789ffc75416b29af14c97f55f8f8
Megan Faith Seibert-Hughes <meganfaithhughes@gmail.com>
2022-12-02 17:32:29 -0600

    fixed nodemon issue

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -2,9 +2,7 @@
 
 const { app } = require("../src/server.js");
 const supertest = require("supertest");
-const { sequelizeDatabase } = require("../src/models/index.js");
-const { DESCRIBE } = require("sequelize/types/query-types");
-const { INET } = require("sequelize");
+const { sequelizeDatabase } = require("../src/models/index");
 
 const request = supertest(app);
 
@@ -13,29 +11,28 @@ beforeAll(async () => {
   await sequelizeDatabase.sync();
 });
 
-afterAll(async() => {
-    await sequelizeDatabase.drop();
+afterAll(async () => {
+  await sequelizeDatabase.drop();
 });
 
-describe('testing server', () => {
-    it('Reponds with a 404 on an invalid route' async () => {
-        const response = await request.get(/'badroute');
-        expect(response.status).toEqual(404);
+describe("testing server", () => {
+  it("Reponds with a 404 on an invalid route", async () => {
+    const response = await request.get("/badroute");
+    expect(response.status).toEqual(404);
+  });
+
+  it("responds with a 404 on an invalid method", async () => {
+    const response = await request.post("/");
+    expect(response.status).toEqual(404);
+  });
+
+  it("creates a new record using POST", async () => {
+    const response = await request.post("/food").send({
+      name: "pizza",
+      calories: 400,
+      type: "CARB",
     });
-
-    it('responds with a 404 on an invalid method', async () => {
-        const response = await request.post('/');
-        expect(response.status).toEqual(404);
-      });
-
-      it('creates a new record using POST', async () => {
-        const response = await request.post('/food').send({
-          name: 'pizza',
-          calories: 400,
-          type: 'CARB',
-        });
-        expect(response.status).toEqual(201);
-        expect(response.body.name).toEqual('pizza');
-      });
-})
-
+    expect(response.status).toEqual(201);
+    expect(response.body.name).toEqual("pizza");
+  });
+});
--- src/models/index.js
+++ src/models/index.js
@@ -8,8 +8,8 @@ const foodSchema = require("./food.schema.js");
 // 'postgres://localhost:5432/api-server'
 // 'postgres://username:password@localhost:5432/api-app'  <-- if you have a username and password
 // will use ternary operator to setup sqlite for testing
-const DATABASE_URL =
-  process.env.NODE_ENV === "test" ? "sqlite:memory" : process.env.DATABASE_URL;
+// const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = "sqlite:memory:";
 
 console.log(DATABASE_URL);
 
@@ -27,7 +27,7 @@ const ClothesModel = clothesSchema(sequelizeDatabase, DataTypes);
 const FoodModel = foodSchema(sequelizeDatabase, DataTypes);
 
 //export the reference to sequelizedDB. the database itself is created within postgres. the sequelizedDatabase is the connection for my ORm to my database. connects from service to database.
-module.exporta = {
+module.exports = {
   sequelizeDatabase,
   ClothesModel,
   FoodModel,
